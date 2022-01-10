be444a1a32086e2223660e18444e783171f9bbae
taotaoli <ltt124356871@126.com>
2022-01-10 22:57:04 +0800

    Create cat_raw_name.py

---
+++ cat_raw_name.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+targetPath = r'D:\work\python\copy_bmp_by_filenum\raw'
+suffix = r'.raw'
+
+dstfile = 'name.txt'
+
+
+def catname():
+    fp = open(dstfile,mode='w')
+    file = os.listdir(targetPath)
+    file.sort(key=lambda x: int((x[:-4].split('_')[-1])[2:]))  # cut suffix, 以 _ 分割文件名，去最后一个str，并cut前两个字符
+    for f in file:
+        f_lower = f.lower()  # 忽略后缀大小写
+        if f_lower.endswith(suffix):
+            fullpath = targetPath + '\\' + f + '\n'
+            fp.write(fullpath)
+
+    print('cat full file name to [', dstfile, '] done')
+    fp.close()
+
+
+if __name__ == '__main__':
+    if not os.path.exists(targetPath):
+        print(targetPath, 'not exist,return !!!')
+        sys.exit(0)
+    catname()
+
